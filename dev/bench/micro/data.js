2f26902363a223491c69c00488f54f0cfb60b982
github-action-benchmark <github@users.noreply.github.com>
2026-03-13 06:13:50 +0000

    add Surge Micro (small) (customSmallerIsBetter) benchmark result for 03a31c12c8ff93d7f2e9716e9ab79448f79cb384

--- dev/bench/micro/data.js
+++ dev/bench/micro/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773296590917,
+  "lastUpdate": 1773382430647,
   "repoUrl": "https://github.com/fintermobilityas/surge",
   "entries": {
     "Surge Micro (small)": [
@@ -371,6 +371,68 @@ window.BENCHMARK_DATA = {
           {
             "name": "chunked bspatch",
             "value": 434.87856600000003,
+            "unit": "ms"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Peter Rekdal Khan-Sunde",
+            "username": "peters",
+            "email": "peter.sunde@gmail.com"
+          },
+          "committer": {
+            "name": "Peter Rekdal Khan-Sunde",
+            "username": "peters",
+            "email": "peter.sunde@gmail.com"
+          },
+          "id": "03a31c12c8ff93d7f2e9716e9ab79448f79cb384",
+          "message": "test(core): stabilize archive delta demoapp regression",
+          "timestamp": "2026-03-06T20:35:23Z",
+          "url": "https://github.com/fintermobilityas/surge/commit/03a31c12c8ff93d7f2e9716e9ab79448f79cb384"
+        },
+        "date": 1773382429815,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Archive create (zstd=3)",
+            "value": 107.974924,
+            "unit": "ms"
+          },
+          {
+            "name": "Archive extract",
+            "value": 75.246537,
+            "unit": "ms"
+          },
+          {
+            "name": "SHA-256 (in-memory)",
+            "value": 5.088487,
+            "unit": "ms"
+          },
+          {
+            "name": "SHA-256 (file)",
+            "value": 5.658599000000001,
+            "unit": "ms"
+          },
+          {
+            "name": "Zstd compress (level=3)",
+            "value": 77.106675,
+            "unit": "ms"
+          },
+          {
+            "name": "Zstd decompress",
+            "value": 12.037653,
+            "unit": "ms"
+          },
+          {
+            "name": "chunked bsdiff",
+            "value": 8630.227118,
+            "unit": "ms"
+          },
+          {
+            "name": "chunked bspatch",
+            "value": 426.68318,
             "unit": "ms"
           }
         ]

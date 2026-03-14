31b7f182925613878eeb8fa1839d0275294cbe31
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 08:55:06 +0000

    add Surge Installers (medium) (customSmallerIsBetter) benchmark result for 8f159edfe42dd5c599f6b1581d03a9c06d070df4

---
+++ dev/bench/installers/data.js
@@ -0,0 +1,60 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1773478506233,
+  "repoUrl": "https://github.com/fintermobilityas/surge",
+  "entries": {
+    "Surge Installers (medium)": [
+      {
+        "commit": {
+          "author": {
+            "name": "Peter Rekdal Khan-Sunde",
+            "username": "peters",
+            "email": "peter.sunde@gmail.com"
+          },
+          "committer": {
+            "name": "Peter Rekdal Khan-Sunde",
+            "username": "peters",
+            "email": "peter.sunde@gmail.com"
+          },
+          "id": "8f159edfe42dd5c599f6b1581d03a9c06d070df4",
+          "message": "bench: track release and installer KPIs",
+          "timestamp": "2026-03-14T07:31:32Z",
+          "url": "https://github.com/fintermobilityas/surge/commit/8f159edfe42dd5c599f6b1581d03a9c06d070df4"
+        },
+        "date": 1773478505361,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "Installer create (online)",
+            "value": 70.87464899999999,
+            "unit": "ms"
+          },
+          {
+            "name": "Installer create (online) size",
+            "value": 4.574581146240234,
+            "unit": "MiB"
+          },
+          {
+            "name": "Installer run (online)",
+            "value": 528.744644,
+            "unit": "ms"
+          },
+          {
+            "name": "Installer create (offline)",
+            "value": 189.256057,
+            "unit": "ms"
+          },
+          {
+            "name": "Installer create (offline) size",
+            "value": 41.73681449890137,
+            "unit": "MiB"
+          },
+          {
+            "name": "Installer run (offline)",
+            "value": 421.661113,
+            "unit": "ms"
+          }
+        ]
+      }
+    ]
+  }
+}
